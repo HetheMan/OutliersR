bf2c80e3df932dcd104891262e15bdc38b5845e2
Zamar <zamar.fazal@edu.uah.es>
2018-04-24 09:49:51 +0200

    dos

--- test.R
+++ test.R
@@ -1,5 +1,6 @@
 library(knitr)
 library(ggplot2)
+library(RANN)
 path <- "./Documento.Rnw"
 data<- read.csv(file="data/Absenteeism_at_work.csv", header=TRUE, sep=";")
 #Queremos encontrar los ID outliers de Edad y 
@@ -17,4 +18,8 @@ dispersion <- ggplot(empleados, aes(x=Age,y=NumAusencias)) + geom_point() + geom
 annotate("rect", xmin = 52, xmax = 58, ymin = 40, ymax = 60, fill="white", colour="red") +
       annotate("text", x=55, y=55, label = paste("R^2 == ", Rcuadrado), parse=T) + annotate("text", x=55, y=50, label = paste("alpha == ",alpha), parse=T) +
       annotate("text", x=55, y=45, label = paste("beta == ",beta), parse=T)
+
+AuxEmpleados = empleados[,c("Age","NumAusencias")]
+kVecinos = nn2(AuxEmpleados, k = 3);
+KVecinos.dists
 
